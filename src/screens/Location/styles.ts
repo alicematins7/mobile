5acf5187216d98549ed86a6a017afbcf6c1dfcc2
alicematins7 <martinsalice280206@gmail.com>
2023-06-20 14:04:41 -0300

    alicve

--- src/screens/Location/styles.ts
+++ src/screens/Location/styles.ts
@@ -1,6 +1,7 @@
- import { Dimensions, StyleSheet} from 'react-native'
+import { Dimensions, StyleSheet } from 'react-native';
+import { colors } from '../../styles/colors';
 
- export const styles = StyleSheet.create({
+export const styles = StyleSheet.create({
     container: {
       flex: 1,
       alignItems: 'center',
@@ -11,8 +12,20 @@
       fontSize: 18,
       textAlign: 'center',
     },
-    map :{
-        width: Dimensions.get('window').width,
-        height: Dimensions.get('window').height
+    map: {
+      width: Dimensions.get("window").width,
+      height: Dimensions.get("window").height
+    },
+    searchContainer: {
+      position: 'absolute',
+      zIndex: 1,
+      width: '90%',
+      top: 10
+    },
+    searchInput: {
+      height: 56,
+      borderRadius: 7,
+      borderWidth: 2,
+      borderColor: colors.third
     }
   });
