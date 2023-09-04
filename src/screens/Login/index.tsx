80427cd5159ec092e5d1e964d16e152e4b52b9fa
alicematins7 <martinsalice280206@gmail.com>
2023-09-04 10:34:09 -0300

    data

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,15 +1,69 @@
-import React from 'react';
-import { 
-    View, KeyboardAvoidingView, Text,
-    TextInput
-} from "react-native";
+import React, { useState, useEffect } from 'react';
+import { View, KeyboardAvoidingView, Text, TextInput, Alert} from "react-native";
 import {styles} from "./styles";
-import { MaterialIcons, FontAwesome5 } from '@expo/vector-icons';
+import { MaterialIcons, FontAwesome5, AntDesign, FontAwesome } from '@expo/vector-icons';
 import { colors } from '../../styles/colors';
-import {ComponentButtonInterface} from '../../components';
+import {ComponentButtonInterface, ComponentLoading} from '../../components';
 import { LoginTypes } from "../../navigation/login.navigation"
-
+import { useAuth } from '../../hooks/auth';
+import { IUserLogin,IAuthenticate } from '../../services/data/User';
+import { apiUser } from '../../services/data';
+import { AxiosError } from 'axios';
+export interface IErrorApi {
+    errors: {
+        rule: string
+        field: string
+        message: string
+    }[]
+}
 export function Login({navigation}: LoginTypes) {
+    const { signIn} = useAuth();
+    const [data, setData] = useState<IAuthenticate>();
+    const [isloading, setIsLoading] = useState(true);
+    async function SignIn() {
+
+}
+    const [data, setData] = useState<IUserLogin>()
+    const [isloading, setIsLoading] = useState(true)
+    function handleChange(item: IUserLogin){
+        setData({...data, ...item})
+    }
+    async function handleLogin() {
+        try {
+            setIsLoading(true) // clicou no botão de salvar
+            if(user?.user, user?.token){
+                const response = await apiUser.register(data)
+                Alert.alert(`${response.data.name}.cadastrado!!`) // crase é usada para concatenar variavel e texto no mesmo conteudo
+                navigation.navigate("Drawer")
+            } else {
+                Alert.alert("Preencha todos os campos")
+            }
+        } catch (error){
+            const err = error as AxiosError
+            const errorData = err.response?.data as IErrorApi
+            let message = ""
+            if (errorData){
+                for ( const iterator of errorData.errors) {
+                    message = `${message} ${iterator.message}`
+                }
+            }
+        Alert.alert(message)
+        } finally {
+            setIsLoading
+        }   
+    }
+    useEffect(() => {
+        setTimeout(() => {
+            setIsLoading(false)
+        }, 500) // aguarda meio segundo, e dá falso
+    }, [])
+    return(
+        <> 
+        {isloading ? ( // se ele for verdadeiro, dá carregando, se for falso, mostra o formulário
+           <ComponentLoading />
+    ) : (  
+    );
+    }
     return(
         <View style={styles.container}>
             <KeyboardAvoidingView>
